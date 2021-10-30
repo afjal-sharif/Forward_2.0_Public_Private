a42471e06bbcb96fad45fd53121fd58ca60d2bca
Afjal Sharif <67854373+afjal-sharif@users.noreply.github.com>
2021-10-30 14:16:15 +0600

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -152,8 +152,9 @@ async def cb_handler(bot: Client, query: CallbackQuery):
             await bot.send_message(query.from_user.id, "Error!!\n\nRequest timed out.\nRestart by using /index")
             return
         input=get_caption.text
+        file_name=media.file_name
         if input == "0":
-            caption= media.file_name
+            caption=file_name
         else:
             caption=input
         break
